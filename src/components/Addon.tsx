df9b8504a62cac2529e13330332dc046a8cd7d9b
antran1245 <antran1245@gmail.com>
2022-12-05 16:18:58 -0800

    add on comp style for desktop and mobile

--- src/components/Addon.tsx
+++ src/components/Addon.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Col, Button } from 'react-bootstrap'
+import { Col, Button, Row } from 'react-bootstrap'
 import '../sass/addon.scss'
 
 type AddonProps = {
@@ -10,9 +10,44 @@ export default function Addon ({setDisplay} : AddonProps) {
     return(
         <Col xs={11} sm={12} lg={8} id="addon">
             <div>
-                <h1>Select your plan</h1>
+                <h1>Pick add-ons</h1>
                 <p className='detail'>You have the option of monthly or yearly billing.</p>
-                
+                <Row>
+                    <Col xs={2} md={2}>
+                        <input type="checkbox"/>
+                    </Col>
+                    <Col>
+                        <p className='title'>Online service</p>
+                        <p className='detail'>Access to multiplayer games</p>
+                    </Col>
+                    <Col xs={2}>
+                        <p className='cost'>+$1/mo</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={2} md={2}>
+                        <input type="checkbox"/>
+                    </Col>
+                    <Col>
+                        <p className='title'>Larger storage</p>
+                        <p className='detail'>Extra 1TB of cloud save</p>
+                    </Col>
+                    <Col xs={2}>
+                        <p className='cost'>+$2/mo</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={2} md={2}>
+                        <input type="checkbox"/>
+                    </Col>
+                    <Col>
+                        <p className='title'>Customizable profile</p>
+                        <p className='detail'>Custom theme on your profile</p>
+                    </Col>
+                    <Col xs={2}>
+                        <p className='cost'>+$2/mo</p>
+                    </Col>
+                </Row>
             </div>
             <div className='button-group'>
                 <p onClick={() => setDisplay('plan')}>Go Back</p>
